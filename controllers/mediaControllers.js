cfbaa6a30ed967ffc5e2b5412792a0998234a535
billy.oles <billythierry104@gmail.com>
2024-11-10 14:21:04 +0700

    benerin /libs/imageKit

--- controllers/mediaControllers.js
+++ controllers/mediaControllers.js
@@ -1,5 +1,5 @@
 const {  PrismaClient } = require("@prisma/client");
-const imageKit = require("../libs/imagekit");
+const imageKit = require("../libs/imageKit");
 const prisma = new PrismaClient();
 
 
